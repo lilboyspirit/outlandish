189599159f426fef12ef637be4faf477e851e4e1
Spirit <spirit@programmer.net>
2024-04-30 19:59:56 +0300

    Add `main_menu`

---
+++ scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+
+func _on_start():
+	pass # Replace with function body.
+
+
+func _on_settings():
+	pass # Replace with function body.
+
+
+func _on_credits():
+	pass # Replace with function body.
+
+
+func _on_quit():
+	get_tree().quit()
