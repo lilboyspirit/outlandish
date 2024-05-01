a5c5c74be28c3016ee5dae29f5f719ad84d2ea0c
Spirit <spirit@programmer.net>
2024-05-01 15:40:22 +0300

    add support for quitting using input

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -1,6 +1,12 @@
 extends Node
 
 
+func _input(event: InputEvent):
+	if event.is_action_pressed("ui_cancel"):
+		get_viewport().set_input_as_handled()
+		_on_quit()
+
+
 func _on_start():
 	pass # Replace with function body.
 
