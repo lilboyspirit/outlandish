327d5cea1ea92c59b37712755e7c22c0116a2a51
Spirit <spirit@programmer.net>
2024-06-14 03:24:39 +0300

    Format

--- autoload/Settings.gd
+++ autoload/Settings.gd
@@ -4,15 +4,6 @@ extends Node
 signal general_settings_updated
 
 
-# General
-var power_saving: bool = false :
-	set(value):
-		power_saving = value
-		emit_signal("general_settings_updated")
-	get:
-		return power_saving
-
-
 func _ready():
 	_on_general_settings_updated()
 	connect("general_settings_updated", _on_general_settings_updated)
@@ -20,3 +11,12 @@ func _ready():
 
 func _on_general_settings_updated():
 	OS.low_processor_usage_mode = power_saving
+
+
+# General
+var power_saving: bool = false :
+	set(value):
+		power_saving = value
+		emit_signal("general_settings_updated")
+	get:
+		return power_saving
